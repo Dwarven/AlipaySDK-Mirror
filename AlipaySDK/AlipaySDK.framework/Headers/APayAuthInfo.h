5f1ebb4c0182f83615c7679c1513c8cd1e4bd6ce
Dwarven YANG <prison.yang@gmail.com>
2020-02-10 18:01:42 +0800

    add AlipaySDK 15.2.0

--- AlipaySDK/AlipaySDK.framework/Headers/APayAuthInfo.h
+++ AlipaySDK/AlipaySDK.framework/Headers/APayAuthInfo.h
@@ -1,9 +1,9 @@
 //
 //  APAuthInfo.h
-//  APAuth
+//  AliSDKDemo
 //
-//  Created by antfin on 17-10-24.
-//  Copyright (c) 2017年 AntFin. All rights reserved.
+//  Created by 方彬 on 14-7-18.
+//  Copyright (c) 2014年 Alipay.com. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -18,6 +18,7 @@
  *  初始化AuthInfo
  *
  *  @param appIDStr     应用ID
+ *  @param productIDStr 产品码 该商户在aboss签约的产品,用户获取pid获取的参数
  *  @param pidStr       商户ID   可不填
  *  @param uriStr       授权的应用回调地址  比如：alidemo://auth
  *
@@ -29,5 +30,4 @@
 
 - (NSString *)description;
 - (NSString *)wapDescription;
-
 @end
